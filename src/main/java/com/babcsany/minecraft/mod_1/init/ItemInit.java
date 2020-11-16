49b6b087704584ddf88cfde993afe629285ed0c7
Ervin Babcsány <ervin@babcsany.com>
2020-11-16 15:49:41 +0100

    1 added, 1 modified src/main

--- src/main/java/com/babcsany/minecraft/mod_1/init/ItemInit.java
+++ src/main/java/com/babcsany/minecraft/mod_1/init/ItemInit.java
@@ -12,5 +12,7 @@ public class ItemInit {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Mod_1.MOD_ID);
 
-
+    public static final RegistryObject<Item> TRURTR = ITEMS.register("trurtr", () -> new Item(
+            (new Item.Properties())
+    ));
 }
